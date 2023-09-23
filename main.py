d4d95190f3e4eb0c89cd123e11fc9e2838de599a
Prajwalkpl <115644599+Prajwalkpl@users.noreply.github.com>
2023-09-23 22:48:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,22 @@ vect= pickle.load(open('vectorizer.pkl','rb'))
 def home():
     return render_template("index.html")
 
+@app.route('/model.html')
+def fun_name():
+    return render_template("model.html")
 
 @app.route('/predict',methods=['POST','GET'])
 def predict():
     form_data = [x for x in request.form.values()]
     output=model.predict(vect.transform(form_data).toarray())
     # print(output)
-    return render_template('index.html',pred="Current Condition: {}".format(output))
+    return render_template('model.html',pred="Current Condition: {}".format(output))
+
+@app.route('/remedy.html')
+def remedy():
+    return render_template('remedy.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
